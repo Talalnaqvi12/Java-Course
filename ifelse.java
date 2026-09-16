4d674af72f390323b5aad8b6c64fdd6761b9102b
Talalnaqvi12 <syedtalal130@gmail.com>
2026-09-16 18:38:05 +0500

    refresh the concept of if else condition

---
+++ ifelse.java
@@ -0,0 +1,47 @@
+public class ifelse {
+
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c =3;
+        if(a > b ){
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is not greater than b");
+        }
+
+
+
+    // conditions with logical and operator 
+
+
+if((a<b )&& (a>c)){
+System.out.println("condtion is true ");
+
+}
+else{
+    System.out.println("Condition is false");
+}
+// condtions with OR operator
+
+
+if((a<b) || (c>b)) {
+
+    //first condtion is true it will not check for second one
+    System.out.println("OR operator condition is true");}
+    else{
+        
+    System.out.println("OR operator condition is false");
+
+    }
+}
+
+
+    }
+
+    
+
+
+
+
+    
